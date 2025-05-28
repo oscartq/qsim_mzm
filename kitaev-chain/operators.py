680cd010c6c7fb384f50e9c806abbc0bf2effcba
oscartq <tornquistoscar@gmail.com>
2025-05-28 12:18:20 +0200

    generated new figures for the kitaev chains

--- kitaev-chain/operators.py
+++ kitaev-chain/operators.py
@@ -46,41 +46,6 @@ def gamma_corr_op(num_sites, siteA, siteB):
 
     return gamma_corr_JW 
 
-def cc_site_op(num_sites):
-    cc_op = [
-        0.0 * (I ^ num_sites)
-    ]
-    return cc_op
-
-def cc_nb_op(num_sites, site1, site2):
-    if abs(site1 - site2) != 1:
-        raise ValueError('Error: Sites have to be nearest neighbour sites. Use corr_op for other sites.')
-    
-    sigma_plus = 0.5*(X + 1.0j*Y)
-    if site1 < site2:
-        cc_op = [ 
-            (I ^ site1) ^ -sigma_plus ^ sigma_plus ^ (I ^ (num_sites - site1 - 2)) 
-        ]
-    elif site1 > site2:
-        cc_op = [
-            (I ^ site2) ^ sigma_plus ^ sigma_plus ^ (I ^ (num_sites - site2 - 2)) 
-        ]
-
-    return cc_op
-
-def cc_edge_op(num_sites, site1, site2):
-    sigma_plus = 0.5*(X + 1.0j*Y)
-    if site1 < site2:
-        cc_op = [
-            sigma_plus ^ (Z ^ (num_sites - 2)) ^ -sigma_plus
-        ]
-    elif site1 > site2:
-        cc_op = [
-            sigma_plus ^ (Z ^ (num_sites - 2)) ^ sigma_plus
-        ]
-            
-    return cc_op
-
 # corr=\
 #     FermionicOp("+_0", register_length = nqbit)@FermionicOp("+_"+str(nqbit-1), register_length = nqbit)\
 #     +FermionicOp("-_0", register_length = nqbit)@FermionicOp("+_"+str(nqbit-1), register_length = nqbit)\
